6f613552544aa03b623621935afb745ce01b98b4
Vitalii Berko <vitalii.v.berko@gmail.com>
2022-12-22 15:34:13 +0200

    Started task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component, Fragment } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    const { value, name } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+  
+  handleOnSubmit =e=>{
+    e.preventDault()
+    console.log(this.state)
+  }
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <Fragment>
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleOnSubmit}>
+          <label htmlFor="">
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label htmlFor="">
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={number}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+      </Fragment>
+    );
+  }
+}
